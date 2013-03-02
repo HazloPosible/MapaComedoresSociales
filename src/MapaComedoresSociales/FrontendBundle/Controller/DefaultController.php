5dc93d3a912c30ecd8c82e223c62d2ad2d26fa94
Sergio <smoya89@gmail.com>
2013-03-02 21:49:30 +0100

    Populate main map v1.0 (without indexing)

--- src/MapaComedoresSociales/FrontendBundle/Controller/DefaultController.php
+++ src/MapaComedoresSociales/FrontendBundle/Controller/DefaultController.php
@@ -1,14 +1,9 @@
 <?php
 
 namespace MapaComedoresSociales\FrontendBundle\Controller;
-use Ivory\GoogleMap\Overlays\InfoWindow;
-
-use Ivory\GoogleMap\Overlays\Animation;
 
 use MapaComedoresSociales\PantryBundle\Form\PantryFilterType;
-
 use MapaComedoresSociales\PantryBundle\Form\PantryType;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -16,51 +11,15 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $pantryForm = $this->createForm(new PantryType());
-        $map = $this->get('ivory_google_map.map');
-        $panControl = $this->get('ivory_google_map.pan_control');
-        $map->setPanControl($panControl);
-        $map->setPanControl(ControlPosition::TOP_LEFT);
 
-        $marker = $this->get('ivory_google_map.marker');
-
-        //@TODO: Use marker clustering with javascript library
-        //@see https://code.google.com/p/google-maps-utility-library-v3
-
-        // Markers TEST
-        // Configure your marker options
-        $marker->setPosition(41.3809434, 2.1676915, true);
-        $marker->setAnimation(Animation::DROP);
-
-        $marker
-                ->setOptions(
-                        array('clickable' => true, 'flat' => true));
-
-        $infoWindow = new InfoWindow();
-        $infoWindow->setContent('CONTENIDO!');
-
-        $marker->setInfoWindow($infoWindow);
-
-        $map->addMarker($marker);
-        $marker = $this->get('ivory_google_map.marker');
-        $marker->setPosition(41.3719434, 2.1625344, true);
-        $marker->setAnimation(Animation::DROP);
-
-        $marker
-                ->setOptions(
-                        array('clickable' => true, 'flat' => true));
-        $infoWindow = new InfoWindow();
-        $infoWindow->setContent('CONTENIDO!');
-
-        $marker->setInfoWindow($infoWindow);
-        $map->addMarker($marker);
+        $map = $this->get('mapacomedoressociales.manager.map')->getMap();
         $pantryFilterForm = $this->createForm(new PantryFilterType());
 
-        return $this
-                ->render('FrontendBundle:Default:index.html.twig',
-                        array('pantryForm' => $pantryForm->createView(),
-                                'map' => $map,
-                                'pantryFilterForm' => $pantryFilterForm
-                                        ->createView()));
+        return $this->render('FrontendBundle:Default:index.html.twig', array(
+                'pantryForm' => $pantryForm->createView(),
+                'map' => $map,
+                'pantryFilterForm' => $pantryFilterForm->createView()
+                )
+        );
     }
 }
-
